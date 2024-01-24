3c335d287ef5ff3bc5d67368795a58146b3e61a3
Omotuwa Ojo <75334653+omotuwaojo@users.noreply.github.com>
2024-01-24 02:29:09 -0800

    first commit

---
+++ flights/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns =[
+   path("", views.index, name="index.html"),
+   path("<int:flight_id>", views.flight, name="flight.html"),
+   path("<int:flight_id>/book", views.book, name="book"),
+   
+]
